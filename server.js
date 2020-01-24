62014df9d6808185b8faad246344bbcb54e8cbf2
Natanael730i <natanaelmuller730@gmail.com>
2020-01-24 18:29:32 -0300

    Implementando GET

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express('');
-const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb+srv://NodeJs:MyDbForLearning@mongodbcomnodejs-8gxkf.gcp.mongodb.net/test?retryWrites=true&w=majority'
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://NodeJs:MyDbForLearning@mongodbcomnodejs-8gxkf.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 MongoClient.connect(uri, (err, client) => {
-    
-    if(err)return console.log(err);
-    db = client.db('NodeJs');
+    if (err) return console.log(err)
+    db = client.db('crud-nodejs') // coloque o nome do seu DB
 
     app.listen(3000, () => {
-        console.log('server runing on port 3000');
-    });
+        console.log('Server running on port 3000')
+    })
+})
 
-});
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
-});
+    res.render('index.ejs')
+})
 
-app.use(bodyParser.urlencoded({ extended : true }));
+app.get('/', (req, res) => {
+    var cursor = db.collection('data').find()
+})
 
-app.post('/show', (req, res)=> {
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
 
-    db.collection('data').save(req.bosy, (err, result) => {
-        if(err)return console.log(err);
-        
-        console.log('Salvo no banco de dados!');
     })
+})
 
+app.post('/show', (req, res) => {
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+    })
 })
